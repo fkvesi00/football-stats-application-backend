eef1e007264acdfb7e66c06815622f426706cd3d
fkvesi00 <fkvesi00@fesb.hr>
2024-01-29 16:48:34 +0100

    adding new route that will give list of all players by clubs, numbers of their app and goals

--- index.js
+++ index.js
@@ -102,6 +102,39 @@ app.get('/matches/getMatchesFormatted',(req, res) => {matches.getMatchesFormatte
 
 app.post('/calculations/formatedTable',  (req, res) => {calculations.formatedTable(req, res, postgres)});
 
+//geetting list of apperances and goals fur jeden club
+app.post('/pga', (req, res) => {
+  const { seasonid, teamid } = req.body;
+
+  postgres
+    .select('player.playerid', 'player.playername')
+    .countDistinct('teammatchplayer.matchid as appearances')  // Use countDistinct to count only distinct matches
+    .count('goal.goalid as goals')
+    .from('playerteamseason as pts')
+    .join('teammatchplayer', function () {
+      this.on('pts.playerid', '=', 'teammatchplayer.playerid').andOn('pts.teamid', '=', 'teammatchplayer.teamid');
+    })
+    .leftJoin('goal', function () {
+      this.on('pts.playerid', '=', 'goal.playerid')
+        .andOn('pts.teamid', '=', 'goal.teamid')
+        .andOn('teammatchplayer.matchid', '=', 'goal.matchid');
+    })
+    .join('player', 'pts.playerid', '=', 'player.playerid')
+    .where({
+      'pts.teamid': teamid,
+      'pts.seasonid': seasonid
+    })
+    .groupBy('player.playerid', 'player.playername')
+    .orderBy('appearances', 'desc')
+    .then(results => {
+      res.json(results);
+      // Process the results here
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    });
+})
 
 app.listen( port, ()=>{
   console.log(`Sluša na ${port}`);
