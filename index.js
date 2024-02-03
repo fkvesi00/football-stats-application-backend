b944e1a23c16aea948f68fcccc5e5ac89b5797dd
fkvesi00 <fkvesi00@fesb.hr>
2024-02-03 14:11:00 +0100

    putting limit on only first 20 scorers

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const port = 5001;
 
 // Use cors middleware with your custom options
 app.use(cors({
-  origin: 'https://main--uma-metkovic.netlify.app', // Replace with your client domain in production
+  origin: [
+    'https://main--uma-metkovic.netlify.app',
+    'https://www.umametkovic.com',
+    'https://umametkovic.com'
+  ], 
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   optionsSuccessStatus: 204,
 }));
@@ -150,10 +154,11 @@ app.post('/scorers', (req, res) => {
     .join('player', 'goal.playerid', '=', 'player.playerid')
     .join('team', 'goal.teamid', '=', 'team.teamid')
     .join('match', 'teammatchplayer.matchid', '=', 'match.matchid')
-    .where('match.seasonid', '=', seasonid)  // Use seasonid from the match table
+    .where('match.seasonid', '=', seasonid)
     .groupBy('player.playerid', 'player.playername', 'team.teamname')
     .havingRaw('count(DISTINCT goal.goalid) > 0')
     .orderBy('goals', 'desc')
+    .limit(20)  // Add this line to limit the results to the first 20
     .then(results => {
       res.json(results);
       // Process the results here
