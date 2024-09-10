0a11a7aa23924e16c060b4f2d903f175b7fad891
fkvesi00 <fkvesi00@fesb.hr>
2024-09-10 20:04:51 +0200

    making route little bit better

--- controllers/clubs.js
+++ controllers/clubs.js
@@ -13,23 +13,49 @@ const getClubsList = async (req, res, postgres) => {
 const getClubGames = (req, res, postgres) => {
   const { teamID, seasonid } = req.body;
 
-  postgres('teamplayingmatch')
+  postgres('teamplayingmatch as tp1')
     .select(
-      'match.matchid',
-      'team.teamname',
-      'match.score',
-      'teamplayingmatch.home',
-      'match.date',
-      'match.time',
-      'team.teamid'
+      'match.matchid as MatchID',
+      'match.date as Date',
+      'team1.teamid as Team1ID',
+      'team1.teamname as Team1Name',
+      'team2.teamid as Team2ID',
+      'team2.teamname as Team2Name',
+      'tp1.home as Team1Home',
+      'match.score as score'
     )
-    .join('match', 'teamplayingmatch.matchid', '=', 'match.matchid')
-    .join('team', 'team.teamid', '=', 'teamplayingmatch.teamid')
-    .where('teamplayingmatch.teamid', teamID)
-    .andWhere('match.seasonid', seasonid)  // Filter by seasonid
-    .then(data => res.json(data))
+    .join('match', 'tp1.matchid', '=', 'match.matchid')
+    .join('team as team1', 'team1.teamid', '=', 'tp1.teamid')
+    .join('teamplayingmatch as tp2', function() {
+      this.on('tp1.matchid', '=', 'tp2.matchid').andOn('tp1.teamid', '<>', 'tp2.teamid');
+    })
+    .join('team as team2', 'team2.teamid', '=', 'tp2.teamid')
+    .where('tp1.teamid', teamID)
+    .andWhere('match.seasonid', seasonid)
+    .then(data => {
+      // Process the data to assign the correct home and away teams
+      const processedData = data.map(game => {
+        const isTeam1Home = game.Team1Home;
+
+        return {
+          MatchID: game.MatchID,
+          Date: game.Date,
+          HomeTeamID: isTeam1Home ? game.Team1ID : game.Team2ID,
+          HomeTeamName: isTeam1Home ? game.Team1Name : game.Team2Name,
+          AwayTeamName: isTeam1Home ? game.Team2Name : game.Team1Name,
+          score: game.score,
+          a_id: isTeam1Home ? game.Team2ID : game.Team1ID,
+          h_id: isTeam1Home ? game.Team1ID : game.Team2ID
+        };
+      });
+
+      res.json(processedData);
+    })
     .catch(err => console.error('Database query error:', err));
 };
+
+
+
 
 
   const addClub = (req,res, postgres) =>{
